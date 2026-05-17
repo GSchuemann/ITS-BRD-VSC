731e3577f1a2f63ffa9114a82dc19ee433e6edf7
Georg Schümann <georg.schuemann@web.de>
2026-05-18 00:03:10 +0200

    flowchart

---
+++ Programs/PrimzahlSiebASM/README.md
@@ -0,0 +1,1 @@
+<img src="./primzahl_flow.svg">
--- Programs/PrimzahlSiebASM/Src/main.go
+++ Programs/PrimzahlSiebASM/Src/main.go
@@ -6,14 +6,10 @@ import (
 
 var primArr = make([]byte, 126)
 var curPos int64 = 2
-var curBytePos = 0
 var primes = make([]int16, 0) //Initialiersrung eines Dynamischen Arrays im Speicher
 
 func main() {
 	sieb()
-	// for i:=2;i<1000;i++ {
-	// TODO Ausgabe vom Array, bzw. dessen Validierung
-	// }
 	abspeichern()
 	for i := 0; i < len(primes); i++ {
 		fmt.Printf("%v ist eine Primzahl!\n", primes[i])
@@ -21,20 +17,21 @@ func main() {
 
 }
 func sieb() {
-	arrPos := curPos % 8
+	curBit := curPos % 8
 	curByte := primArr[int(curPos>>3)]         //lsr 3 ist div 8
-	bitToCheck := (curByte & (0x80 >> arrPos)) //setzt bitToCheck auf 0, wenn das bit 0 und sonst > 0
-	//  bitToCheck := fmt.Sprintf("%08b\n", curByte)[arrPos]
-	// fmt.Println(fmt.Sprintf("%08b ,%v\n", curByte, arrPos))
+	bitToCheck := (curByte & (0x80 >> curBit)) //setzt bitToCheck auf 0, wenn das bit 0 und sonst > 0
+	//  bitToCheck := fmt.Sprintf("%08b\n", curByte)[curBit]
+	// fmt.Println(fmt.Sprintf("%08b ,%v\n", curByte, curBit))
 	if bitToCheck == 0 {
 		// fmt.Printf("%v ist eine Primzahl \n", curPos)
 		for i := (curPos * curPos); i < int64(len(primArr)*8); i += curPos {
 			b := int(i >> 3)
-			bit := (i + 8) & 0x07 //&(AND) 0x07 ist % 8
+			bit := i & 0x07 //&(AND) 0x07 ist % 8
 			mask := getMask(int(bit))
 			primArr[b] = primArr[b] | byte(mask)
 		}
 	}
+
 	curPos += 1
 	if curPos < int64(len(primArr)*8) {
 		sieb()
@@ -42,11 +39,11 @@ func sieb() {
 }
 func abspeichern() {
 	for i := 2; i < len(primArr)*8; i++ {
-		arrPos := i % 8
+		curBit := i % 8
 		curByte := primArr[i>>3]
-		curBit := (curByte & (0x80 >> arrPos))
+		bitToCheck := (curByte & (0x80 >> curBit))
 
-		if curBit == 0 {
+		if bitToCheck == 0 {
 			primes = append(primes, int16(i))
 		}
 	}
