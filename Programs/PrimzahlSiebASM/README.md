b5e2bb661d4df1f5855b2e59d78796958968ff92
Georg Schümann <Georg.schuemann@web.de>
2026-05-18 19:16:15 +0200

    Code clean up

--- Programs/PrimzahlSiebASM/README.md
+++ Programs/PrimzahlSiebASM/README.md
@@ -23,4 +23,57 @@ Dann Halbwort belegt mit Anazahl der Elemente
 Dann soviele Halbwörter, wie Anzahl der Elemente
 
 Beispiel Flowchart, letzter Punkt ist nicht korrekt abgebildet:
-<img src="./primzahl_flow.svg">
+
+<img src="./primzahl_flow.svg" alt="Flowchart für das Programm">
+
+Beispie Code (Go), Java wurde nicht benutzt, weil Bytes da signed sind.
+
+```golang
+package main
+
+import (
+	"fmt"
+)
+
+var primArr = make([]byte, 126)
+var curPos int64 = 2
+var primes = make([]int16, 0) //Initialiersrung eines Dynamischen Arrays im Speicher
+
+func main() {
+	sieb()
+	abspeichern()
+	for i := 0; i < len(primes); i++ {
+		fmt.Printf("%v ist eine Primzahl!\n", primes[i])
+	}
+
+}
+func sieb() {
+	curBit := curPos % 8
+	curByte := primArr[int(curPos>>3)]         //lsr 3 ist div 8
+	bitToCheck := (curByte & (0x80 >> curBit)) //setzt bitToCheck auf 0, wenn das bit 0 und sonst > 0
+
+	if bitToCheck == 0 {
+		// fmt.Printf("%v ist eine Primzahl \n", curPos)
+		for i := (curPos * curPos); i < int64(len(primArr)*8); i += curPos {
+			curBit = i & 0x07 //&(AND) 0x07 ist % 8
+			curByte = byte(i >> 3)
+			primArr[curByte] = primArr[curByte] | (0x80 >> curBit)
+		}
+	}
+	curPos += 1
+	if curPos < int64(len(primArr)*8) {
+		sieb()
+	}
+}
+func abspeichern() {
+	for i := 2; i < len(primArr)*8; i++ {
+		curBit := i % 8
+		curByte := primArr[i>>3]
+		bitToCheck := (curByte & (0x80 >> curBit))
+
+		if bitToCheck == 0 {
+			primes = append(primes, int16(i))
+		}
+	}
+}
+```
--- Programs/PrimzahlSiebASM/Src/main.go
+++ Programs/PrimzahlSiebASM/Src/main.go
@@ -20,18 +20,15 @@ func sieb() {
 	curBit := curPos % 8
 	curByte := primArr[int(curPos>>3)]         //lsr 3 ist div 8
 	bitToCheck := (curByte & (0x80 >> curBit)) //setzt bitToCheck auf 0, wenn das bit 0 und sonst > 0
-	//  bitToCheck := fmt.Sprintf("%08b\n", curByte)[curBit]
-	// fmt.Println(fmt.Sprintf("%08b ,%v\n", curByte, curBit))
+
 	if bitToCheck == 0 {
 		// fmt.Printf("%v ist eine Primzahl \n", curPos)
 		for i := (curPos * curPos); i < int64(len(primArr)*8); i += curPos {
-			b := int(i >> 3)
-			bit := i & 0x07 //&(AND) 0x07 ist % 8
-			mask := getMask(int(bit))
-			primArr[b] = primArr[b] | byte(mask)
+			curBit = i & 0x07 //&(AND) 0x07 ist % 8
+			curByte = byte(i >> 3)
+			primArr[curByte] = primArr[curByte] | (0x80 >> curBit)
 		}
 	}
-
 	curPos += 1
 	if curPos < int64(len(primArr)*8) {
 		sieb()
@@ -49,41 +46,3 @@ func abspeichern() {
 	}
 
 }
-func getMask(bit int) int {
-	return 0x80 >> bit
-	// switch bit {
-	// case 0:
-	// 	return 0x80
-	// case 1:
-	// 	return 0x40
-	// case 2:
-	// 	return 0x20
-	// case 3:
-	// 	return 0x10
-	// case 4:
-	// 	return 0x08
-	// case 5:
-	// 	return 0x04
-	// case 6:
-	// 	return 0x02
-	// case 7:
-	// 	return 0x01
-	// default:
-	// 	return 0x00
-	// }
-}
-
-// func div(arrPos int64) {
-// 	if arrPos == 0 {
-
-// 		return
-// 	}
-// 	byteToCheck := primArr[int(arrPos/8)]
-// 	bitToCheck := fmt.Sprintf("%08b\n", byteToCheck)[arrPos]
-// 	if bitToCheck == 49 {
-// 		div(arrPos - 1)
-// 	}
-// 	if ((curPos + 2) % arrPos) == 0 {
-// 		fmt.Printf("%v ist keine Primzahl!\n", curPos+2)
-// 	}
-// }
